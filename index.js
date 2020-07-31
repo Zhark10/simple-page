d516c436d3ab7436614a2f3ab5609b0d837f872c
Zhark10 <verygoodman18@yandex.ru>
2020-07-31 23:04:10 +0300

    minor changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
+
 const mountains = document.getElementById("mountains");
 const water = document.getElementById("water");
 const sands = document.getElementById("sands");
@@ -6,5 +8,5 @@ const about_me = document.getElementById("about_me");
 
 window.addEventListener("scroll", function () {
   const value = window.scrollY;
-  mountains.style.top = value * 0.5 + 'px';
+  sun.style.top = -value * 0.5 + 'px';
 });
